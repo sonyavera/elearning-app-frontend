91f2b6291c5b0b1b44ed6502192181af56e2a783
sonyavera <sonyavgould@gmail.com>
2020-11-28 11:41:19 -0700

    babel wepback styled components

---
+++ src/components/CourseContent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { useState, useContext } from 'react'
+
+
+function CourseContent() {
+    
+    
+
+
+    return(
+        <h1>Hi</h1>
+    )
+
+}
+
+export default CourseContent;
---
+++ src/components/TaskListDropdown.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { useState, useContext } from 'react'
+
+
+function TaskListDropdown() {
+    
+    
+
+
+    return(
+        <h1>Task List</h1>
+    )
+
+}
+
+export default TaskListDropdown;
